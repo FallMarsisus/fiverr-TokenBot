b0e7184603f78493cfaa3a7142ab7af90bdccf44
Marsisus <simonlncln@gmail.com>
2023-05-11 10:10:49 +0200

    final order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import json
 from views import ButtonsTokens
+import asyncio
 
 
 intents = discord.Intents.default()
@@ -57,9 +58,10 @@ async def on_ready():
 async def on_member_join(member):
     global data
     
+    """
     welcomeChat = await member.guild.fetch_channel("1105797683374989392")
     await welcomeChat.send(f":information_source: Welcome {member.mention} ! If you've been invited by someone, type **{prefix}join @itsusername**.")
-
+    """
 
 @client.event
 async def on_message(message):
@@ -67,8 +69,17 @@ async def on_message(message):
     # if message.author == client.user:
     #     return
 
-    if message.content.startswith(f"{prefix}join"):
+    if message.content.startswith(f"{prefix}acceptrules"):
         await message.delete()
+
+        role = discord.utils.get(message.author.guild.roles, name="Accepted Rules")
+
+        if len(message.content.split(" ")) == 1:
+            await message.author.add_roles(role)
+            welcomeMessage = await message.channel.send(f":white_check_mark: {message.author.name}, you're now in the server, enjoy !", delete_after=100)
+            await asyncio.sleep(10)
+            await welcomeMessage.delete()
+            return
         
         exists = verifyIfUser(message.author.id)
         if exists[0]:
@@ -94,20 +105,46 @@ async def on_message(message):
         
         try:
             if data['users'][index]["invited"]:
-                await message.channel.send(":no_entry: You have already been invited to the server")
-            
+                welcomeMessage = await message.channel.send(f":no_entry: {message.author.name}, You have already been invited to the server, please retry", delete_after=10)
+                await asyncio.sleep(10)
+                await welcomeMessage.delete()
+                
+                return
         except KeyError:
             pass
+
+        date = message.created_at
+        try:
+            if data["users"][indexI]["lastInviteDate"] != date.day:
+                data["users"][indexI]["todayInviteCount"] = 0
+            if data["users"][indexI]["todayInviteCount"] > 9:
+                welcomeMessage = await message.channel.send(f":no_entry: Sorry {message.author.name}, the person who invited you has reach its 10 invitations per day limit, please retry", delete_after=10)
+                await asyncio.sleep(10)
+                await welcomeMessage.delete()
+                return
+        except KeyError:
+            data["users"][indexI]["todayInviteCount"] = 0
+            pass
+
+        data["users"][indexI]["lastInviteDate"] = date.day
+        data["users"][indexI]["todayInviteCount"] +=1
         
         data["users"][index]["invited"] = True
         data["users"][indexI]["coins"] += 2
         data["users"][indexI]["inviteCount"] += 1
         data["users"][index]["name"] = message.author.name
         
+
         saveData()
         
-        await message.channel.send(":information_source:")
+        await message.author.add_roles(role)
         
+        welcomeMessage = await message.channel.send(f":information_source: {message.author.name}, your invitation was taken, thanks ! Welcome on the server !", delete_after=100)
+        await asyncio.sleep(10)
+        await welcomeMessage.delete()
+
+    if message.channel.id == 1106122173321592842 and message.author != client.user:
+        await message.delete()
 
     if message.content.startswith(f"{prefix}invite"):
         await message.delete()
@@ -121,7 +158,7 @@ async def on_message(message):
 
         try:
             if len(data["users"][index]["inviteLink"]) > 0:
-                await message.channel.send(f"{message.author.name}, you can't have more than one invite link pending at the same time. Your invite code is {data['users'][index]['inviteLink']}")
+                await message.channel.send(f":information_source: {message.author.name}, you can't have more than one invite link pending at the same time. Your invite code is {data['users'][index]['inviteLink']}")
                 return
         except KeyError:
             pass
@@ -131,8 +168,9 @@ async def on_message(message):
 
         data["users"][index]["inviteLink"] = str(invite_link)
         data['users'][index]["inviteCount"] = 0
+        data["users"][index]["name"] = message.author.name
 
-        await message.channel.send(f"{message.author.name}, here is your invite link : {invite_link}")
+        await message.channel.send(f":information_source: {message.author.name}, here is your invite link : {invite_link}")
 
 
         saveData()
@@ -149,7 +187,7 @@ async def on_message(message):
 
             qr = discord.File("bankqr.png")
             await message.channel.send(f""":information_source: Scan the code and pay {value*0.1} USD in order to get your {value} :coin:
-                                       They"ll be given to you after 1 to 10 hour(s )""", file=qr)
+                                       They'll be given to you after 1 to 10 hour(s)""", file=qr)
             
             exists = verifyIfUser(message.author.id)
             if exists[0]:
@@ -186,7 +224,7 @@ async def on_message(message):
         count = message.content.split(" ")[2]
         
         if not count.isdigit():
-            await message.channel.send("Wrong Count")
+            await message.channel.send(":no_entry: Wrong Count")
             return
         
         if user[2:-1].isdigit():     
@@ -207,7 +245,7 @@ async def on_message(message):
 
         await message.channel.send(f":white_check_mark: Successfuly Added {count} coins to {user} ! New Sold : {data['users'][index]['coins']} :coin:.")
         
-    if message.content.startswith(f"{prefix}coins"):
+    if message.content.startswith(f"{prefix}stats"):
 
         exists = verifyIfUser(message.author.id)
 
@@ -218,12 +256,28 @@ async def on_message(message):
             data["users"].append({})
             makeProfile(int(message.author.id))
 
-        embed = discord.Embed(title="Your Dashboard", description="See your coins stats !", color=0xfdff75)
+        embed = discord.Embed(title="Your Dashboard", description="See your stats !", color=0xfdff75)
         embed.set_author(name=str(data["users"][index]["coins"]) + " coins", icon_url="https://emoji.discadia.com/emojis/23fea924-f44f-4061-b0f0-b4dab898b19c.PNG")
         embed.add_field(name="Coins :", value= str(data["users"][index]["coins"]) + " :coin:", inline=True)
         embed.add_field(name="Coins being verified :", value=str(data["users"][index]["toadd"]) + " :coin:", inline=True)
+        try:
+            embed.add_field(name="Invitations count :", value=str(data["users"][index]["inviteCount"]) + ":envelope:", inline=False)
+            embed.add_field(name="Invitation link", value=data["users"][index]["inviteLink"])
+        except KeyError:
+            pass
+
         await message.channel.send(embed=embed)
 
+    if message.content.startswith(f"{prefix}help"):
+        mainEmbed = discord.Embed(title="Need Help ?", description="You're on the right place !", color=0x00ffaa)
+        mainEmbed.set_footer(text="A bot made by Marsisus", icon_url="https://marsisus.alwaysdata.net/wp-content/uploads/2023/02/moiLama-150x150.jpg")
+        
+        simpleCommandsEmbed = discord.Embed(title='Main Commands', description="The minimal commands you have to know to use the bot.", color=0x33aaff)
+        simpleCommandsEmbed.add_field(inline=False, name="**$buy** _amount_", value="Sends a link for you to pay tokens and tells an admin that you used the command")
+        simpleCommandsEmbed.add_field(inline=False, name="**$invite**", value="If not already generated, creates a new personnal invite link for the server, every time you invite someone, you earn 2 :coin:")
+        simpleCommandsEmbed.add_field(inline=False, name="**$coins**", value="Shows all the stats you need")
+
+        await message.channel.send(embeds=(mainEmbed, simpleCommandsEmbed))
     
 
 client.run('MTEwMzM5Njc4MDg2NDk4MzExMA.GCmsvA.m9Lf70YWrSN3B0mqoU71V2wfVKjqUukANRCr_U')
